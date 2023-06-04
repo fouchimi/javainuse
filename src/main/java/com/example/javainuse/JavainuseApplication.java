f96c07a7cbb84961da5a09a06bffaa7d291f59c0
Fouchimi Nvuh Ousmane <fousmane@Fouchimis-Air.lan>
2023-06-04 10:12:20 -0400

    Inital commit for Java Jwt tutorial

---
+++ src/main/java/com/example/javainuse/JavainuseApplication.java
@@ -0,0 +1,13 @@
+package com.example.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavainuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavainuseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javainuse/JavainuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javainuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavainuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
